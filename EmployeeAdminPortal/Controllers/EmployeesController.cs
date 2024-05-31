21d9f6b6465189e1d02b686013f174352db41778
aashwinbhushal961 <aashwinbhushal961@outlook.com>
2024-05-31 08:33:54 +0530

    Add-migration-Add-EmployeeDTO

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,47 @@
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext=dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var employess= dbContext.Employees.ToList();
+            return Ok(employess);
+        }
+        [HttpGet]   
+        public IActionResult GetEmployee(int id)
+        {
+            var employess = dbContext.Employees.ToList();
+            return Ok(employess);
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee()
+            {
+                Email = addEmployeeDto.Email,
+                Name = addEmployeeDto.Name,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary,
+            };
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+            return Ok(employeeEntity);
+        }
+    }
+}
---
+++ EmployeeAdminPortal/Models/AddEmployeeDto.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+
+namespace EmployeeAdminPortal.Models
+{
+    public class AddEmployeeDto
+    {
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
